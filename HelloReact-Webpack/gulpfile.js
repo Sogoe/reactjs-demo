a85c2b99adcf50ab2d0c96657625d2436d12cbba
Sogoe <sogoe@qq.com>
2015-07-31 17:25:45 +0800

    add gulp task uglify

--- HelloReact-Webpack/gulpfile.js
+++ HelloReact-Webpack/gulpfile.js
@@ -2,6 +2,8 @@
  * Created by Lyy on 2015/7/31.
  */
 var gulp = require("gulp");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
 var webpack = require("webpack");
 var webpackConfig = require("./webpack.config");
 
@@ -10,3 +12,12 @@ gulp.task("webpack", function() {
 
     });
 });
+
+gulp.task("uglify", ["webpack"], function() {
+    gulp.src("./build/hello.bundle.js")
+        .pipe(uglify())
+        .pipe(rename("hello.bundle.min.js"))
+        .pipe(gulp.dest("./build"))
+});
+
+gulp.task("default", ["webpack"]);
